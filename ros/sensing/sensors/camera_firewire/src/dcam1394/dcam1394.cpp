ac804dc28afdd0866ac8d1bcf017031d4640eabd
Peter Polidoro <peterpolidoro@gmail.com>
2010-05-25 17:33:38 -0700

    Add one to DC1394_VIDEO_MODE_NUM

--- ros/sensing/sensors/camera_firewire/src/dcam1394/dcam1394.cpp
+++ ros/sensing/sensors/camera_firewire/src/dcam1394/dcam1394.cpp
@@ -224,7 +224,7 @@ dcam::getVendor(size_t i)
 }
 
 
-static const char *modestrings[DC1394_VIDEO_MODE_NUM] =
+static const char *modestrings[DC1394_VIDEO_MODE_NUM+1] =
   {
     "DC1394_VIDEO_MODE_160x120_YUV444",
     "DC1394_VIDEO_MODE_320x240_YUV422",
