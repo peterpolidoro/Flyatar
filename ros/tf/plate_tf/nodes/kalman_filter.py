7c5130fa5685d7e36df381449eb6982a5671f58b
Peter Polidoro <peterpolidoro@gmail.com>
2010-06-23 15:31:44 -0700

    Use kalman filter to calculate positions and velocities

--- ros/tf/plate_tf/nodes/kalman_filter.py
+++ ros/tf/plate_tf/nodes/kalman_filter.py
@@ -4,20 +4,36 @@ import roslib
 roslib.load_manifest('plate_tf')
 import rospy
 
-import cv, numpy
+import cv
 from geometry_msgs.msg import PoseStamped
 
 class KalmanFilter:
     def __init__(self):
         self.kal = cv.CreateKalman(4,2,0)
+        cv.SetIdentity(self.kal.transition_matrix)
+        cv.SetIdentity(self.kal.measurement_matrix)
+        self.measurement = cv.CreateMat(2,1,cv.GetElemType(self.kal.state_pre))
         self.t_previous = None
 
     def update(self,z,t):
         self.t_current = t
-        self.update_dt()
+        if self.update_dt():
+            cv.KalmanPredict(self.kal)
+            self.measurement[0,0] = z[0]
+            self.measurement[1,0] = z[1]
+            state_post = cv.KalmanCorrect(self.kal,self.measurement)
+            x = state_post[0,0]
+            y = state_post[1,0]
+            vx = state_post[2,0]
+            vy = state_post[3,0]
+            rospy.logwarn("x = %s, y = %s, vx = %s, vy = %s" % (x,y,vx,vy))
 
     def update_dt(self):
+        status = False
         if self.t_previous is not None:
             self.dt = self.t_current - self.t_previous
-            rospy.logwarn("dt = %s", str(self.dt))
+            self.kal.transition_matrix[0,2] = self.dt
+            self.kal.transition_matrix[1,3] = self.dt
+            status = True
         self.t_previous = self.t_current
+        return status
