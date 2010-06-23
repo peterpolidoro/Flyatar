de1f3ccd89edecab41c2dee3b5444c2ad82643cc
Peter Polidoro <peterpolidoro@gmail.com>
2010-06-23 16:15:21 -0700

    Print debugging messages

--- ros/tf/plate_tf/nodes/kalman_filter.py
+++ ros/tf/plate_tf/nodes/kalman_filter.py
@@ -22,6 +22,9 @@ class KalmanFilter:
         x = y = vx = vy = None
         if self.update_dt():
             cv.KalmanPredict(self.kal)
+            Q = self.kal.process_noise_cov[0,0]
+            R = self.kal.measurement_noise_cov[0,0]
+            rospy.logwarn("Q = %s, R = %s" % (str(Q),str(R))
             self.measurement[0,0] = z[0]
             self.measurement[1,0] = z[1]
             state_post = cv.KalmanCorrect(self.kal,self.measurement)
