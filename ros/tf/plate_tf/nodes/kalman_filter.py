ea42d3cd0cf328dfb68181354c7550816a9785ea
Peter Polidoro <peterpolidoro@gmail.com>
2010-06-23 17:01:46 -0700

    Try another cov ratio

--- ros/tf/plate_tf/nodes/kalman_filter.py
+++ ros/tf/plate_tf/nodes/kalman_filter.py
@@ -12,7 +12,7 @@ class KalmanFilter:
         self.kal = cv.CreateKalman(4,2,0)
         cv.SetIdentity(self.kal.transition_matrix)
         cv.SetIdentity(self.kal.measurement_matrix)
-        cv.SetIdentity(self.kal.process_noise_cov, 1)
+        cv.SetIdentity(self.kal.process_noise_cov, 100)
         cv.SetIdentity(self.kal.measurement_noise_cov, 0.00001)
         self.measurement = cv.CreateMat(2,1,cv.GetElemType(self.kal.state_pre))
         self.t_previous = None
