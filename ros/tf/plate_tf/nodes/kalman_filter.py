a872642881493260078d60434f22ce8023e5746e
Peter Polidoro <peterpolidoro@gmail.com>
2010-06-23 18:09:29 -0700

    Try using observed instantaneous velocity as measurement

--- ros/tf/plate_tf/nodes/kalman_filter.py
+++ ros/tf/plate_tf/nodes/kalman_filter.py
@@ -9,12 +9,14 @@ from geometry_msgs.msg import PoseStamped
 
 class KalmanFilter:
     def __init__(self):
-        self.kal = cv.CreateKalman(4,2,0)
+        # self.kal = cv.CreateKalman(4,2,0)
+        self.kal = cv.CreateKalman(4,4,0)
         cv.SetIdentity(self.kal.transition_matrix)
         cv.SetIdentity(self.kal.measurement_matrix)
-        cv.SetIdentity(self.kal.process_noise_cov, 1000)
-        cv.SetIdentity(self.kal.measurement_noise_cov, 0.0001)
-        self.measurement = cv.CreateMat(2,1,cv.GetElemType(self.kal.state_pre))
+        cv.SetIdentity(self.kal.process_noise_cov, 100)
+        cv.SetIdentity(self.kal.measurement_noise_cov, 0.001)
+        # self.measurement = cv.CreateMat(2,1,cv.GetElemType(self.kal.state_pre))
+        self.measurement = cv.CreateMat(4,1,cv.GetElemType(self.kal.state_pre))
         self.t_previous = None
         self.x_previous = None
         self.y_previous = None
@@ -29,18 +31,21 @@ class KalmanFilter:
             # rospy.logwarn("Q = %s, R = %s" % (str(Q),str(R)))
             self.measurement[0,0] = z[0]
             self.measurement[1,0] = z[1]
-            rospy.logwarn("measurement[0,0] = %s" % str(self.measurement[0,0]))
-            rospy.logwarn("measurement[1,0] = %s" % str(self.measurement[1,0]))
+
+            if self.x_previous is not None:
+                self.measurement[2,0] = (z[0] - self.x_previous)/self.dt
+                self.measurement[3,0] = (z[1] - self.y_previous)/self.dt
+            #     rospy.logwarn("x_velocity = %s, y_velocity = %s" % (str(x_velocity),str(y_velocity)))
+
+            # rospy.logwarn("measurement[0,0] = %s" % str(self.measurement[0,0]))
+            # rospy.logwarn("measurement[1,0] = %s" % str(self.measurement[1,0]))
+
             state_post = cv.KalmanCorrect(self.kal,self.measurement)
             x = state_post[0,0]
             y = state_post[1,0]
             vx = state_post[2,0]
             vy = state_post[3,0]
 
-            if self.x_previous is not None:
-                x_velocity = (x - self.x_previous)/self.dt
-                y_velocity = (y - self.y_previous)/self.dt
-                rospy.logwarn("x_velocity = %s, y_velocity = %s" % (str(x_velocity),str(y_velocity)))
             self.x_previous = x
             self.y_previous = y
 
