9fd8e166322f1a77b42ef003d78a74def7faafce
Peter Polidoro <peterpolidoro@gmail.com>
2010-07-01 11:50:27 -0700

    Add some debugging calculations

--- ros/tf/plate_tf/nodes/PlateStageTransforms.py
+++ ros/tf/plate_tf/nodes/PlateStageTransforms.py
@@ -17,51 +17,32 @@ class Transforms:
             except (tf.LookupException, tf.ConnectivityException):
                 pass
 
-        rospy.logwarn("trans = %s" % (str(self.trans)))
-        rospy.logwarn("rot = %s" % (str(self.rot)))
+        # rospy.logwarn("trans = %s" % (str(self.trans)))
+        # rospy.logwarn("rot = %s" % (str(self.rot)))
 
         self.T = tf.transformations.translation_matrix(self.trans)
         self.R = tf.transformations.quaternion_matrix(self.rot)
-        self.M1 = tf.transformations.concatenate_matrices(self.R, self.T)
-        self.M2 = tf.transformations.concatenate_matrices(self.T, self.R)
-        rospy.logwarn("T = \n%s" % (str(self.T)))
-        rospy.logwarn("R = \n%s" % (str(self.R)))
-        rospy.logwarn("M1 = \n%s" % (str(self.M1)))
-        rospy.logwarn("M2 = \n%s" % (str(self.M2)))
-
-        # (intrinsic_matrix,distortion_coeffs) = StageParameters.intrinsic("rect")
-        # (rvec,tvec) = StageParameters.extrinsic("plate")
-        # intrinsic_matrix = cvNumpy.mat_to_array(intrinsic_matrix)
-        # rvec = cvNumpy.mat_to_array(rvec).squeeze()
-        # tvec = cvNumpy.mat_to_array(tvec).squeeze()
-
-        # rvec_angle = numpy.linalg.norm(rvec)
-        # R = tf.transformations.rotation_matrix(rvec_angle,rvec)
-        # T = tf.transformations.translation_matrix(tvec)
-
-        # Wsub = numpy.zeros((3,3))
-        # Wsub[:,:-1] = R[:-1,:-2]
-        # Wsub[:,-1] = T[:-1,-1]
-
-        # M = intrinsic_matrix
-        # # Makes with respect to Stage coordinate system instead of Undistorted
-        # M[:-1,-1] = 0
-
-        # Hinv = numpy.dot(M,Wsub)
-        # Hinv = Hinv/Hinv[-1,-1]
-        # H = numpy.linalg.inv(Hinv)
-        # self.Hinv = Hinv
-        # self.H = H
-        # # rospy.logwarn("H = \n%s", str(H))
-
+        self.M = tf.transformations.concatenate_matrices(self.R, self.T)
+        self.Minv = numpy.linalg.inv(self.M)
+        # rospy.logwarn("T = \n%s" % (str(self.T)))
+        # rospy.logwarn("R = \n%s" % (str(self.R)))
+        # rospy.logwarn("M1 = \n%s" % (str(self.M1)))
+        Xsrc = [0,10]
+        Ysrc = [0,10]
+        Zsrc = [0,0]
+        Hsrc = [1,1]
+        plate_points = numpy.array([Xsrc,Ysrc,Zsrc])
+        stage_points = numpy.dot(self.M,plate_points)
+        rospy.logwarn("plate_points = \n%s" % (str(plate_points)))
+        rospy.logwarn("stage_points = \n%s" % (str(plate_points)))
 
     def plate_to_stage(self,req):
         point_count = min(len(req.Xsrc),len(req.Ysrc))
         Xsrc = list(req.Xsrc)
         Ysrc = list(req.Ysrc)
-        Zsrc = [1]*point_count
+        Zsrc = [0]*point_count
         plate_points = numpy.array([Xsrc,Ysrc,Zsrc])
-        stage_points = numpy.dot(self.Hinv,plate_points)
+        stage_points = numpy.dot(self.M,plate_points)
         Xdst = stage_points[0,:]
         Ydst = stage_points[1,:]
         return {'Xdst': Xdst,
@@ -71,9 +52,9 @@ class Transforms:
         point_count = min(len(req.Xsrc),len(req.Ysrc))
         Xsrc = list(req.Xsrc)
         Ysrc = list(req.Ysrc)
-        Zsrc = [1]*point_count
+        Zsrc = [0]*point_count
         stage_points = numpy.array([Xsrc,Ysrc,Zsrc])
-        plate_points = numpy.dot(self.H,stage_points)
+        plate_points = numpy.dot(self.Minv,stage_points)
 
         Xdst = plate_points[0,:]
         Ydst = plate_points[1,:]
