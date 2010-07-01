fdaf52971aac1d6cc9ba99aa821ca8d99a954bb3
Peter Polidoro <peterpolidoro@gmail.com>
2010-07-01 12:16:33 -0700

    More debugging messages

--- ros/tf/plate_tf/nodes/PlateStageTransforms.py
+++ ros/tf/plate_tf/nodes/PlateStageTransforms.py
@@ -13,18 +13,30 @@ class Transforms:
         self.transforms_initialized = False
         while not self.transforms_initialized:
             try:
-                (self.trans,self.rot) = self.tf_listener.lookupTransform("Plate", "Stage", rospy.Time.now())
+                (self.trans1,self.rot1) = self.tf_listener.lookupTransform("Plate", "Stage", rospy.Time.now())
+                (self.trans2,self.rot2) = self.tf_listener.lookupTransform("Stage", "Plate", rospy.Time.now())
                 self.transforms_initialized = True
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 pass
 
-        # rospy.logwarn("trans = %s" % (str(self.trans)))
-        # rospy.logwarn("rot = %s" % (str(self.rot)))
+        rospy.logwarn("trans1 = %s" % (str(self.trans1)))
+        rospy.logwarn("rot1 = %s" % (str(self.rot1)))
+        rospy.logwarn("trans2 = %s" % (str(self.trans2)))
+        rospy.logwarn("rot2 = %s" % (str(self.rot2)))
 
-        self.T = tf.transformations.translation_matrix(self.trans)
-        self.R = tf.transformations.quaternion_matrix(self.rot)
-        self.M = tf.transformations.concatenate_matrices(self.R, self.T)
-        self.Minv = numpy.linalg.inv(self.M)
+        self.T1 = tf.transformations.translation_matrix(self.trans1)
+        self.R1 = tf.transformations.quaternion_matrix(self.rot1)
+        self.M11 = tf.transformations.concatenate_matrices(self.R1, self.T1)
+        self.M11inv = numpy.linalg.inv(self.M11)
+        self.M12 = tf.transformations.concatenate_matrices(self.T1, self.R1)
+        self.M12inv = numpy.linalg.inv(self.M12)
+
+        self.T2 = tf.transformations.translation_matrix(self.trans2)
+        self.R2 = tf.transformations.quaternion_matrix(self.rot2)
+        self.M21 = tf.transformations.concatenate_matrices(self.R2, self.T2)
+        self.M21inv = numpy.linalg.inv(self.M21)
+        self.M22 = tf.transformations.concatenate_matrices(self.T2, self.R2)
+        self.M22inv = numpy.linalg.inv(self.M22)
         # rospy.logwarn("T = \n%s" % (str(self.T)))
         # rospy.logwarn("R = \n%s" % (str(self.R)))
         Xsrc = [0,10]
@@ -32,10 +44,16 @@ class Transforms:
         Zsrc = [0,0]
         Hsrc = [1,1]
         plate_points = numpy.array([Xsrc,Ysrc,Zsrc,Hsrc])
-        rospy.logwarn("M = \n%s" % (str(self.M)))
+        # rospy.logwarn("M = \n%s" % (str(self.M)))
         rospy.logwarn("plate_points = \n%s" % (str(plate_points)))
-        stage_points = numpy.dot(self.M,plate_points)
-        rospy.logwarn("stage_points = \n%s" % (str(stage_points)))
+        stage_points = numpy.dot(self.M11,plate_points)
+        rospy.logwarn("stage_points11 = \n%s" % (str(stage_points)))
+        stage_points = numpy.dot(self.M12,plate_points)
+        rospy.logwarn("stage_points12 = \n%s" % (str(stage_points)))
+        stage_points = numpy.dot(self.M21,plate_points)
+        rospy.logwarn("stage_points21 = \n%s" % (str(stage_points)))
+        stage_points = numpy.dot(self.M22,plate_points)
+        rospy.logwarn("stage_points22 = \n%s" % (str(stage_points)))
 
         plate_point = PointStamped()
         plate_point.header.frame_id = "Plate"
