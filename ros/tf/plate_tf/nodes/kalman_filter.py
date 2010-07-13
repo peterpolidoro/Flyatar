3c6a36ecc267e2ebded7d35cbf522491295a99ba
Peter Polidoro <peterpolidoro@gmail.com>
2010-07-13 10:06:12 -0700

    Decrease cov of angle measurements

--- ros/tf/plate_tf/nodes/kalman_filter.py
+++ ros/tf/plate_tf/nodes/kalman_filter.py
@@ -19,10 +19,10 @@ class KalmanFilter:
         cv.SetIdentity(self.kal.measurement_noise_cov, 0.001)
         # self.kal.measurement_noise_cov[2,2] = 40
         # self.kal.measurement_noise_cov[3,3] = 40
-        self.kal.measurement_noise_cov[2,2] = 1000
+        self.kal.measurement_noise_cov[2,2] = 100
         self.kal.measurement_noise_cov[3,3] = 40
         self.kal.measurement_noise_cov[4,4] = 40
-        self.kal.measurement_noise_cov[5,5] = 1000
+        self.kal.measurement_noise_cov[5,5] = 100
         # self.measurement = cv.CreateMat(2,1,cv.GetElemType(self.kal.state_pre))
         # self.measurement = cv.CreateMat(4,1,cv.GetElemType(self.kal.state_pre))
         self.measurement = cv.CreateMat(6,1,cv.GetElemType(self.kal.state_pre))
