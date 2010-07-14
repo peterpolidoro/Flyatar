265478b526d083d4985b17645305fcfc2678e599
Peter Polidoro <peterpolidoro@gmail.com>
2010-07-14 12:10:30 -0700

    Print more debugging messages

--- ros/tf/plate_tf/nodes/choose_orientation.py
+++ ros/tf/plate_tf/nodes/choose_orientation.py
@@ -58,10 +58,8 @@ class ChooseOrientation:
 
         if diff_vel_ang < diff_vel_ang_flipped:
             vel_ang_vote_on_flipped = False
-            # rospy.logwarn("diff_vel_ang = %s" % (str(diff_vel_ang)))
         else:
             vel_ang_vote_on_flipped = True
-            # rospy.logwarn("diff_vel_ang_flipped = %s" % (str(diff_vel_ang_flipped)))
         # rospy.logwarn("ref_ang = %s, orient_ang = %s, orient_ang_flipped = %s, diff_vel_ang = %s, diff_vel_ang_flipped = %s" % (str(ref_ang),str(orient_ang*180/math.pi),str(orient_ang_flipped*180/math.pi),str(diff_vel_ang*180/math.pi),str(diff_vel_ang_flipped*180/math.pi)))
 
         if self.orient_ang_previous is not None:
@@ -87,6 +85,17 @@ class ChooseOrientation:
             # rospy.logwarn("orient_ang = %s" % (str(orient_ang)))
             # rospy.logwarn("orient_ang_flipped = %s" % (str(orient_ang_flipped)))
             rospy.logwarn("disagreement_count = %s" % (str(self.disagreement_count)))
+
+            if vel_ang_vote_on_flipped:
+                rospy.logwarn("diff_vel_ang_flipped = %s" % (str(diff_vel_ang_flipped)))
+            else:
+                rospy.logwarn("diff_vel_ang = %s" % (str(diff_vel_ang)))
+
+            if prev_ang_vote_on_flipped:
+                rospy.logwarn("prev_vel_ang_flipped = %s" % (str(prev_vel_ang_flipped)))
+            else:
+                rospy.logwarn("prev_vel_ang = %s" % (str(prev_vel_ang)))
+
             if self.disagreement_count < self.disagreement_count_limit:
                 flipped = prev_ang_vote_on_flipped
             else:
