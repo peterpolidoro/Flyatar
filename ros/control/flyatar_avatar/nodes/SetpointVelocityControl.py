eb1fe9a510593bc15f3b22f4cf23b9847dd22724
Peter Polidoro <peterpolidoro@gmail.com>
2010-07-22 18:27:32 -0700

    Do not change moving_to_setpoint unless not tracking

--- ros/control/flyatar_avatar/nodes/SetpointVelocityControl.py
+++ ros/control/flyatar_avatar/nodes/SetpointVelocityControl.py
@@ -465,7 +465,6 @@ class SetpointControl:
 
                 self.radius_error,self.theta_error = self.find_robot_setpoint_error()
                 if not self.on_setpoint_radius:
-                    self.moving_to_setpoint = False
                     vel_mag = self.find_radius_vel_mag()
                     self.set_path_to_setpoint(vel_mag)
                     self.sc_ok_to_publish = True
