c3541ee60a14d4a4423f78e5f24567eaf2e2934e
Peter Polidoro <peterpolidoro@gmail.com>
2010-07-29 16:45:58 -0700

    Change debugging messages

--- ros/actuation/stage_tf/nodes/StageUpdate.py
+++ ros/actuation/stage_tf/nodes/StageUpdate.py
@@ -61,18 +61,18 @@ class StageUpdate:
   def stage_commands_callback(self,data):
     if self.initialized:
 
-      rospy.logwarn ("data.position_control = %s" % (str(data.position_control)))
-      rospy.logwarn ("data.lookup_table_correct = %s" % (str(data.lookup_table_correct)))
-      rospy.logwarn ("data.velocity_control = %s" % (str(data.velocity_control)))
       if data.position_control:
+        rospy.logwarn ("data.position_control = %s" % (str(data.position_control)))
         self.update_position = True
         self.update_velocity = False
         self.lookup_table_correct = False
       elif data.lookup_table_correct:
+        rospy.logwarn ("data.lookup_table_correct = %s" % (str(data.lookup_table_correct)))
         self.update_position = False
         self.update_velocity = False
         self.lookup_table_correct = True
       elif data.velocity_control:
+        # rospy.logwarn ("data.velocity_control = %s" % (str(data.velocity_control)))
         self.update_position = False
         self.update_velocity = True
         self.lookup_table_correct = False
