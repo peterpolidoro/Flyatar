544c9b0dfcd990997a042bff7b8966398dd93a9d
Peter Polidoro <peterpolidoro@gmail.com>
2010-09-13 13:12:35 -0700

    Removed comments and lookup_table_velocity_correct from StageCommunicator

--- ros/actuation/stage/nodes/StageCommunicator.py
+++ ros/actuation/stage/nodes/StageCommunicator.py
@@ -8,11 +8,10 @@ import threading
 
 class StageCommunicator():
     def __init__(self):
-        print "Opening Flyatar stage device..."
+        rospy.loginfo("Opening Flyatar stage device...")
         self.dev = StageDevice.StageDevice()
         self.dev.print_values()
         self.response = Stage_StateResponse()
-        self.response.lookup_table_move_in_progress = False
         self.reentrant_lock = threading.Lock()
 
     def _fill_response(self,return_state):
@@ -29,79 +28,31 @@ class StageCommunicator():
 
     def close(self):
         self.dev.close()
-        print "Flyatar stage device closed."
+        rospy.loginfo("Flyatar stage device closed.")
 
     def get_stage_state(self,req):
-        # return_state = self.dev.return_state()
         with self.reentrant_lock:
             return_state = self.dev.get_state()
             self._fill_response(return_state)
         return self.response
 
     def set_stage_velocity(self,req):
-        # x_velocity = req.x_velocity
-        # y_velocity = req.y_velocity
-        # if abs(x_velocity) < self.min_velocity:
-        #     x_velocity = 0
-        # if abs(y_velocity) < self.min_velocity:
-        #     y_velocity = 0
-        # return_state = self.dev.update_velocity(x_velocity,y_velocity)
         x_vel_list = req.x_velocity
         y_vel_list = req.y_velocity
-        # if abs(x_velocity) < self.min_velocity:
-        #     x_velocity = 0
-        # if abs(y_velocity) < self.min_velocity:
-        #     y_velocity = 0
-        # rospy.logwarn ("data.velocity_control = %s" % (str(data.velocity_control)))
-        self.update_position = False
-        self.update_velocity = True
-        self.lookup_table_correct = False
-
         with self.reentrant_lock:
             return_state = self.dev.update_velocity(x_vel_list,y_vel_list)
             self._fill_response(return_state)
         return self.response
 
     def set_stage_position(self,req):
-        # x_position = req.x_position
-        # y_position = req.y_position
-        # x_velocity = req.x_velocity
-        # y_velocity = req.y_velocity
-        # return_state = self.dev.update_position(x_position,x_velocity,y_position,y_velocity)
         x_pos_list = req.x_position
         y_pos_list = req.y_position
         x_vel_list = req.x_velocity
         y_vel_list = req.y_velocity
-        # point_count = min(len(x_pos_list),len(y_pos_list),len(x_vel_list),len(y_vel_list))
-        # rospy.logwarn("point_count = %s" % (str(point_count)))
         with self.reentrant_lock:
             return_state = self.dev.update_position(x_pos_list,x_vel_list,y_pos_list,y_vel_list)
             self._fill_response(return_state)
         return self.response
-
-    def stage_lookup_table_correct(self,req):
-        x_vel_list = req.x_velocity
-        y_vel_list = req.y_velocity
-        with self.reentrant_lock:
-            return_state = self.dev.lookup_table_vel_correct(x_vel_list,y_vel_list)
-            self._fill_response(return_state)
-        return self.response
-
-    # def stage_lookup_table_move(self,req):
-    #     x_pos_list = [120,140,120,100,120,100,100,140,140]*3
-    #     x_vel_list = [20]*len(x_pos_list)
-    #     y_pos_list = [100,120,140,120,100,100,140,140,100]*3
-    #     y_vel_list = [20]*len(y_pos_list)
-
-    #     # x_pos_list = [120,140,120,100,120]
-    #     # x_vel_list = [20,20,20,20,20]
-    #     # y_pos_list = [100,120,140,120,100]
-    #     # y_vel_list = [20,20,20,20,20]
-
-    # with self.reentrant_lock:
-    #     return_state = self.dev.lookup_table_move(x_pos_list,x_vel_list,y_pos_list,y_vel_list)
-    #     self._fill_response(return_state)
-    #     return self.response
 
 if __name__ == '__main__':
     rospy.init_node('StageCommunicator', anonymous=True)
@@ -109,8 +60,6 @@ if __name__ == '__main__':
     s_gss = rospy.Service('get_stage_state', Stage_State, sc.get_stage_state)
     s_ssv = rospy.Service('set_stage_velocity', Stage_State, sc.set_stage_velocity)
     s_ssp = rospy.Service('set_stage_position', Stage_State, sc.set_stage_position)
-    s_sltc = rospy.Service('stage_lookup_table_correct', Stage_State, sc.stage_lookup_table_correct)
-    # s_sltm = rospy.Service('stage_lookup_table_move', Stage_State, sc.stage_lookup_table_move)
 
     while not rospy.is_shutdown():
         rospy.spin()
