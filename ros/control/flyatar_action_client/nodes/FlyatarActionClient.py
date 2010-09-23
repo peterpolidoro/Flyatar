ae9f653fe549891b3d960a24ddda899283f46272
Peter Polidoro <peterpolidoro@gmail.com>
2010-09-23 13:25:32 -0700

    Added flyatar_action_client package

---
+++ ros/control/flyatar_action_client/nodes/FlyatarActionClient.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+import roslib; roslib.load_manifest('flyatar_action_client')
+import rospy
+
+# Brings in the SimpleActionClient
+import actionlib
+
+# Brings in the messages used by the stage_action_server action, including the
+# goal message and the result message.
+import stage_action_server.msg
+
+def flyatar_action_client():
+    # Creates the SimpleActionClient, passing the type of the action
+    # to the constructor.
+    client = actionlib.SimpleActionClient('flyatar_action_client', stage_action_server.msg.UpdateStagePositionAction)
+
+    # Waits until the action server has started up and started
+    # listening for goals.
+    client.wait_for_server()
+
+    # Creates a goal to send to the action server.
+    goal = stage_action_server.msg.UpdateStagePositionGoal()
+    goal.x_position = [100,150,150,100,100]
+    goal.y_position = [100,100,150,150,100]
+    goal.velocity_magnitude = [50]
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
+    # Waits for the server to finish performing the action.
+    client.wait_for_result()
+
+    # Prints out the result of executing the action
+    result = client.get_result()
+    rospy.logwarn("result = %s" % (str(result)))
+
+if __name__ == '__main__':
+    try:
+        # Initializes a rospy node so that the SimpleActionClient can
+        # publish and subscribe over ROS.
+        rospy.init_node('FlyatarActionClient')
+        flyatar_action_client()
+    except rospy.ROSInterruptException:
+        print "program interrupted before completion"
