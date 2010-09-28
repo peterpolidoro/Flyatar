43508bdaa536dab7525b00fd174ac45c3c740496
Peter Polidoro <peterpolidoro@gmail.com>
2010-09-28 14:57:55 -0700

    More name error corrections

--- ros/gui/image_gui/nodes/DrawPrimatives.py
+++ ros/gui/image_gui/nodes/DrawPrimatives.py
@@ -9,13 +9,13 @@ import CvPrimatives
 class CenteredCircle:
     def __init__(self,center,radius,color,thickness):
         self.draw_object = image_gui.msg.DrawObject()
-        self.draw_object.center = center
+        self.draw_object.object_center = center
 
         self.circle = CvPrimatives.Circle((0,0),radius,color,thickness)
         self.draw_object.circle_list = [self.circle]
 
     def change_center(self,center):
-        self.draw_object.center = center
+        self.draw_object.object_center = center
 
     def change_radius(self,radius):
         self.circle.radius = radius
