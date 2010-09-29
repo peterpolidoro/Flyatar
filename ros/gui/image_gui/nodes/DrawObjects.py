c6c0b48c126833fcbb0a8716f42e7862fa0f74a5
Peter Polidoro <peterpolidoro@gmail.com>
2010-09-28 18:28:49 -0700

    Use tf to update objects drawn on image

--- ros/gui/image_gui/nodes/DrawObjects.py
+++ ros/gui/image_gui/nodes/DrawObjects.py
@@ -8,41 +8,65 @@ import image_gui.msg
 import Colors
 import CvPrimatives
 import DrawPrimatives
-from geometry_msgs.msg import PoseStamped
+import tf
+from geometry_msgs.msg import PointStamped
 
 class DrawObjects:
     def __init__(self):
+        self.initialized = False
+        self.display_frame = "ImageRect"
         self.draw_objects_pub = rospy.Publisher("DrawObjects/image_rect", image_gui.msg.DrawObjects)
         self.draw_objects = image_gui.msg.DrawObjects()
+
+        self.dt = rospy.get_param("Draw_Update_dt","0.010")
+        self.rate = rospy.Rate(1/self.dt)
+
+        self.tf_listener = tf.TransformListener()
+        self.plate_image_origin = PointStamped()
+        self.plate_image_origin.header.frame_id = "PlateImage"
+        self.plate_image_origin.point.x = 0
+        self.plate_image_origin.point.y = 0
+        self.plate_image_origin.point.z = 0
+        self.robot_image_origin = PointStamped()
+        self.robot_image_origin.header.frame_id = "RobotImage"
+        self.robot_image_origin.point.x = 0
+        self.robot_image_origin.point.y = 0
+        self.robot_image_origin.point.z = 0
 
         self.colors = Colors.Colors()
 
         self.origin = CvPrimatives.Point(0,0)
         self.robot_marker = DrawPrimatives.CenteredCircle(self.origin.point,25,self.colors.blue,2)
 
-        self.robot_image_pose_sub = rospy.Subscriber('ImagePose/Robot',PoseStamped,self.handle_robot_image_pose)
+        self.tf_listener.waitForTransform(self.display_frame, "PlateImage", rospy.Time(0), rospy.Duration(4.0))
+        self.plate_image_origin_display_frame = self.tf_listener.transformPoint(self.display_frame,
+                                                                                self.plate_image_origin)
 
-        self.plate_origin_marker = DrawPrimatives.Axes(CvPrimatives.Point(300,300).point)
+        self.plate_origin_marker = DrawPrimatives.Axes((self.plate_image_origin_display_frame.point.x,
+                                                        self.plate_image_origin_display_frame.point.y).point)
 
         self.draw_objects.draw_object_list = [self.plate_origin_marker.draw_object,
                                               self.robot_marker.draw_object]
         self.draw_objects.show_all = False
         self.draw_objects.hide_all = False
+        self.initialized = True
 
-        self.robot_marker_x = 0
-        self.robot_marker_y = 0
+    def update(self):
+        if self.initialized:
+            try:
+                self.robot_image_origin_display_frame = self.tf_listener.transformPoint(self.display_frame,
+                                                                                        self.robot_image_origin)
+            except (tf.LookupException, tf.ConnectivityException):
+                pass
 
-        self.rate = rospy.Rate(10)
-
-    def handle_robot_image_pose(self,data):
-        self.robot_marker_x += 1
-        self.robot_marker_y += 2
-        self.robot_marker.change_center(CvPrimatives.Point(self.robot_marker_x,self.robot_marker_y).point)
-        self.draw_objects_pub.publish(self.draw_objects)
+            self.robot_marker.change_center(CvPrimatives.Point(self.robot_image_origin_display_frame.point.x,
+                                                               self.robot_image_origin_display_frame.point.y).point)
+            self.draw_objects_pub.publish(self.draw_objects)
 
 if __name__ == '__main__':
     rospy.init_node('DrawObjects')
     do = DrawObjects()
 
     while not rospy.is_shutdown():
-        rospy.spin()
+        do.update()
+        do.rate.sleep()
