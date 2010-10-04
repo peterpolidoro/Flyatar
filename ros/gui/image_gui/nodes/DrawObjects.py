1f5a6d1205c54a7528ccb547ca6cdab8c0a0ccc9
Peter Polidoro <peterpolidoro@gmail.com>
2010-10-04 10:49:52 -0700

    Convert radius values to camera coordinates before drawing

--- ros/gui/image_gui/nodes/DrawObjects.py
+++ ros/gui/image_gui/nodes/DrawObjects.py
@@ -10,6 +10,7 @@ import CvPrimatives
 import DrawPrimatives
 import tf
 from geometry_msgs.msg import PointStamped
+import math
 
 class DrawObjects:
     def __init__(self):
@@ -21,9 +22,33 @@ class DrawObjects:
         self.dt = rospy.get_param("Draw_Update_dt","0.010")
         self.rate = rospy.Rate(1/self.dt)
 
-        self.in_bounds_radius = rospy.get_param('in_bounds_radius',1)
+        self.in_bounds_radius_plate = rospy.get_param('in_bounds_radius',1)
+        self.axis_length_plate = 4
 
         self.tf_listener = tf.TransformListener()
+
+        rospy.wait_for_service('plate_to_camera')
+        try:
+            self.plate_to_camera = rospy.ServiceProxy('plate_to_camera', PlateCameraConversion)
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+
+        self.plate_camera_origin = PointStamped()
+        self.plate_camera_origin.header.frame_id = "Camera"
+        self.plate_camera_point = PointStamped()
+        self.plate_camera_point.header.frame_id = "Camera"
+        Xsrc = [0,self.in_bounds_radius_plate,self.axis_length_plate]
+        Ysrc = [0,0,0]
+        response = self.plate_to_camera(Xsrc,Ysrc)
+        x0 = self.plate_origin_camera.point.x = response.Xdst[0]
+        y0 = self.plate_origin_camera.point.y = response.Ydst[0]
+        x1 = self.plate_point_camera.point.x = response.Xdst[1]
+        y1 = self.plate_point_camera.point.y = response.Ydst[1]
+        x2 = response.Xdst[1]
+        y2 = response.Ydst[1]
+        self.in_bounds_radius_camera = math.sqrt((x1-x0)**2 + (y1-y0)**2)
+        self.axis_length_camera = math.sqrt((x2-x0)**2 + (y2-y0)**2)
+
         self.plate_image_origin = PointStamped()
         self.plate_image_origin.header.frame_id = "PlateImage"
         self.plate_image_origin.point.x = 0
@@ -34,6 +59,13 @@ class DrawObjects:
         self.robot_image_origin.point.x = 0
         self.robot_image_origin.point.y = 0
         self.robot_image_origin.point.z = 0
+        self.robot_image_origin = PointStamped()
+        self.robot_image_origin.header.frame_id = "RobotImage"
+        self.robot_image_origin.point.x = 0
+        self.robot_image_origin.point.y = 0
+        self.robot_image_origin.point.z = 0
+
+
 
         self.colors = Colors.Colors()
 
@@ -48,9 +80,9 @@ class DrawObjects:
 
         self.plate_origin_marker = DrawPrimatives.Axes(self.plate_origin_primatives.point)
 
-        self.robot_marker = DrawPrimatives.CenteredCircle(self.origin.point,25,self.colors.blue,2)
+        self.robot_marker = DrawPrimatives.CenteredCircle(self.origin.point,self.axis_length_camera,self.colors.blue,2)
 
-        self.in_bounds_marker = DrawPrimatives.CenteredCircle(self.plate_origin_primatives.point,self.in_bounds_radius,self.colors.yellow,1)
+        self.in_bounds_marker = DrawPrimatives.CenteredCircle(self.plate_origin_primatives.point,self.in_bounds_radius_camera,self.colors.yellow,1)
 
         self.draw_objects.draw_object_list = [self.plate_origin_marker.draw_object,
                                               self.robot_marker.draw_object,
